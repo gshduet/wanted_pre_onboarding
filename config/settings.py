a53b06f0ad4a5f45fae4c8447e85343e06192416
gshduet <modis2199@gmail.com>
2022-04-14 15:47:16 +0900

    ADD: initial modeling

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from my_settings import DATABASES, SECRET_KEY
+from my_settings import DATABASES, SECRET_KEY, ALGORITHM
 
 import pymysql
 
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
+ALGORITHM  = ALGORITHM
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -47,7 +48,9 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-
+    'core',
+    'users',
+    'products'
 ]
 
 INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
