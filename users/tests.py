a1ceacea3445336ec3440cb103db0c1c1ab71622
gshduet <modis2199@gmail.com>
2022-04-15 21:45:47 +0900

    ADD : Unittest
    users 앱 라우터 연결
    유저 회원가입 API 및 로그인 API에 관한 유닛테스트 코드 작성, 테스트 통과 확인

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,372 @@
-from django.test import TestCase
+import json
+import jwt
+import bcrypt
 
-# Create your tests here.
+from config.settings import SECRET_KEY, ALGORITHM
+from django.test     import TransactionTestCase, Client
+
+from users.models    import User
+
+
+class SignUpTest(TransactionTestCase):
+    def setUp(self):
+        User.objects.create(
+            id       = 1,
+            name     = 'test_name_01',
+            password = '1Q2W3e4r!',
+            nickname = 'test_name_01'
+        )
+    
+    def tearDown(self):
+        User.objects.all().delete()
+
+    # 회원가입 성공
+    def test_SignUp_success(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_02',
+            'password' : '1Q2W3e4r!'
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(),{'MESSAGE' : 'SUCCESS'})
+        self.assertEqual(response.status_code, 201)
+
+    # 회원가입 실패 : 회원 아이디 중복
+    def test_SignUp_Failure_UserExist(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_01',
+            'password' : '1Q2W3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'NAME_EXISTS'})
+        self.assertEqual(response.status_code, 409)
+
+    # 회원가입 실패 : 회원가입 시 비밀번호 미입력
+    def test_SignUp_Failure_KeyError(self):
+        client = Client()
+
+        new_user = {
+            'password' : '1Q2W3e4r!'
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'KEY_ERROR'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 회원가입 시 아이디 미입력
+    def test_SignUp_Failure_KeyError(self):
+        client = Client()
+
+        new_user = {
+            'name' : 'test_name_03',
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'KEY_ERROR'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 아이디 생성조건 불충족 (알파벳으로 시작)
+    def test_SignUp_Failure_Valid_Name_01(self):
+        client = Client()
+
+        new_user = {
+            'name'     : '04_test_name',
+            'password' : '1Q2W3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_NAME'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 아이디 생성조건 불충족 (아이디 최소길이(8글자) 미달)
+    def test_SignUp_Failure_Valid_Name_02(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_04',
+            'password' : '1Q2W3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_NAME'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 아이디 생성조건 불충족 (아이디 최대길이(15글자) 초과)
+    def test_SignUp_Failure_Valid_Name_03(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04_fail',
+            'password' : '1Q2W3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_NAME'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 아이디 생성조건 불충족 (허용 된 '-', '_', '.' 이외의 특수문자 사용)
+    def test_SignUp_Failure_Valid_Name_04(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04@',
+            'password' : '1Q2W3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_NAME'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 비밀번호 생성조건 불충족 (적어도 하나 이상의 대문자 알파벳을 포함)
+    def test_SignUp_Failure_Valid_Password_01(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04',
+            'password' : '1q2w3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_PASSWORD'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 비밀번호 생성조건 불충족 (적어도 하나 이상의 소문자 알파벳을 포함)
+    def test_SignUp_Failure_Valid_Password_02(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04',
+            'password' : '1Q2W3E4R!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_PASSWORD'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 비밀번호 생성조건 불충족 (적어도 하나 이상의 숫자를 포함)
+    def test_SignUp_Failure_Valid_Password_03(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04',
+            'password' : 'QWERqwer!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_PASSWORD'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 비밀번호 생성조건 불충족 (적어도 하나 이상의 특수문자 포함)
+    def test_SignUp_Failure_Valid_Password_04(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04',
+            'password' : '1Q2W3e4r1Q2W3e4r' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_PASSWORD'})
+        self.assertEqual(response.status_code, 400)
+
+    # 회원가입 실패 : 비밀번호 생성조건 불충족 (비밀번호 최소길이(8글자) 미달)
+    def test_SignUp_Failure_Valid_Password_05(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04',
+            'password' : '1Q2W3e!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_PASSWORD'})
+        self.assertEqual(response.status_code, 400)
+    
+    # 회원가입 실패 : 비밀번호 생성조건 불충족 (비밀번호 최대길이(21글자) 초과)
+    def test_SignUp_Failure_Valid_Password_06(self):
+        client = Client()
+
+        new_user = {
+            'name'     : 'test_name_04',
+            'password' : '1Q2W3e4r!1Q2W3e4r!1Q2W3e4r!' 
+        }
+
+        response = client.post(
+            '/users/signup', json.dumps(new_user), content_type = 'application/json'
+        )
+
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALD_PASSWORD'})
+        self.assertEqual(response.status_code, 400)
+
+
+class SignInTest(TransactionTestCase):
+    def setUp(self):
+        User.objects.create(
+            id       = 1,
+            name     = 'test_name_01',
+            password = bcrypt.hashpw(
+                '1Q2W3e4r!'.encode('utf-8'), bcrypt.gensalt()
+            ).decode('utf-8'),
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    # 로그인 성공
+    def test_SignIn_Success(self):
+        client = Client()
+
+        user    = {
+            'name'     : 'test_name_01',
+            'password' : '1Q2W3e4r!'
+        }
+
+        response = client.post(
+            '/users/signin', json.dumps(user), content_type = 'application/json'
+        )
+        access_token = jwt.encode({'user_id':1}, SECRET_KEY, algorithm = ALGORITHM)
+
+        self.assertEqual(
+            response.json(),
+            {
+                'MESSAGE':'SUCCESS', 'ACCESS_TOKEN': access_token
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+    # 로그인 실패 : 존재하지 않는 유저
+    def test_SignIn_Failure_User_DoesNotExist(self):
+        client = Client()
+
+        user    = {
+            'name'     : 'test_name_02',
+            'password' : '1Q2W3e4r!'
+        }
+
+        response = client.post(
+            '/users/signin', json.dumps(user), content_type = 'application/json'
+        )
+        access_token = jwt.encode({'user_id':1}, SECRET_KEY, algorithm = ALGORITHM)
+
+        self.assertEqual(
+            response.json(),{'MESSAGE':'USER_DOES_NOT_EXIST'}
+        )
+        self.assertEqual(response.status_code, 404)
+
+    # 로그인 실패 : 로그인 시 아이디 미입력
+    def test_SignIn_Failure_KeyError_01(self):
+        client = Client()
+
+        user    = {
+            'password' : '1Q2W3e4r!'
+        }
+
+        response = client.post(
+            '/users/signin', json.dumps(user), content_type = 'application/json'
+        )
+        access_token = jwt.encode({'user_id':1}, SECRET_KEY, algorithm = ALGORITHM)
+
+        self.assertEqual(
+            response.json(),{'MESSAGE':'KEY_ERROR'}
+        )
+        self.assertEqual(response.status_code, 400)
+
+    # 로그인 실패 : 로그인 시 비밀번호 미입력
+    def test_SignIn_Failure_KeyError_02(self):
+        client = Client()
+
+        user    = {
+            'name'     : 'test_name_01'
+        }
+
+        response = client.post(
+            '/users/signin', json.dumps(user), content_type = 'application/json'
+        )
+        access_token = jwt.encode({'user_id':1}, SECRET_KEY, algorithm = ALGORITHM)
+
+        self.assertEqual(
+            response.json(),{'MESSAGE':'KEY_ERROR'}
+        )
+        self.assertEqual(response.status_code, 400)
+
+    # 로그인 실패 : 틀린 아이디
+    def test_SignIn_Failure_INVALID_USER_01(self):
+        client = Client()
+
+        user    = {
+            'name'     : 'test_name_02',
+            'password' : '1Q2W3e4r!'
+        }
+
+        response = client.post(
+            '/users/signin', json.dumps(user), content_type = 'application/json'
+        )
+        access_token = jwt.encode({'user_id':1}, SECRET_KEY, algorithm = ALGORITHM)
+
+        self.assertEqual(
+            response.json(),{'MESSAGE':'INVALID_USER'}
+        )
+        self.assertEqual(response.status_code, 401)
+
+    # 로그인 실패 : 틀린 비밀번호
+    def test_SignIn_Failure_INVALID_USER_01(self):
+        client = Client()
+
+        user    = {
+            'name'     : 'test_name_01',
+            'password' : '1Q2W3e4r@'
+        }
+
+        response = client.post(
+            '/users/signin', json.dumps(user), content_type = 'application/json'
+        )
+        access_token = jwt.encode({'user_id':1}, SECRET_KEY, algorithm = ALGORITHM)
+
+        self.assertEqual(
+            response.json(),{'MESSAGE':'INVALID_USER'}
+        )
+        self.assertEqual(response.status_code, 401)
--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ import jwt
 
 from django.http     import JsonResponse
 from django.views    import View
-from django.db.utils import IntegrityError
 from django.db       import transaction
 
 from config.settings import SECRET_KEY, ALGORITHM
@@ -21,23 +20,25 @@ class SignUpView(View):
             password = data['password']
 
             # 유저가 사용하고자 하는 아이디를 정규식을 통해 검증
-            # 알파벳으로 시작하며 알파벳, 숫자, 일부 특수문자('-', '_', '.')을 사용한 8~15자 사용가능
-            if not re.match('^[a-zA-Z][a-zA-Z0-9-_.]{8,15}$'):
-                return JsonResponse({"MESSAGE" : "INVALD_NAME"}, status=400)
+            # 알파벳으로 시작하며 알파벳, 숫자, 일부 특수문자('-', '_', '.')을 포함한 8~15자 사용가능
+            if not re.match('^[a-zA-Z][a-zA-Z0-9-_.]{8,15}$', name):
+                return JsonResponse({'MESSAGE' : 'INVALD_NAME'}, status=400)
 
             # 유저가 사용하고자 하는 비밀번호를 정규식으로 검증
-            # 알파벳, 숫자, 일부 특수문자를 사용한 8~20자 사용가능
-            if not re.match("^[a-zA-Z0-9!@#$%^&*()\-_=+?~`]{8,20}$", password):
-                return JsonResponse({"MESSAGE" : "INVALD_PASSWORD"}, status=400)
+            # 적어도 대문자 알파벳, 소문자 알파벳, 숫자, 일부 특수문자를 포함한 8~20자 사용가능
+            if not re.match(
+                '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*\-_=+?~`])[A-Za-z0-9!@#$%^&*\-_=+?~`]{8,20}$', password
+            ):
+                return JsonResponse({'MESSAGE' : 'INVALD_PASSWORD'}, status=400)
 
             # 유저가 사용하고자 하는 아이디가 이미 존재할 경우 에러 반환
             if User.objects.filter(name=name).exists():
-                return JsonResponse({"MESSAGE" : "NAME_EXISTS"}, status=400)
+                return JsonResponse({'MESSAGE' : 'NAME_EXISTS'}, status=409)
 
             # 유저가 입력한 비밀번호를 bcrypt를 활용해 암호화
             hashed_password = bcrypt.hashpw(
-                password.encode("utf-8"), bcrypt.gensalt()
-            ).decode("utf-8")
+                password.encode('utf-8'), bcrypt.gensalt()
+            ).decode('utf-8')
 
             # 유저가 입력한 아이디, 암호화 된 비밀번호, 닉네임을 DB에 입력
             # 초기 닉네임은 유저의 아이디와 동일
@@ -62,13 +63,14 @@ class SignInView(View):
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
             # 아이디와 비밀번호가 알맞게 입력된다면 DB 상 입력되어 있는 해당 유저의 ID를 jwt로 복호화 해 생성
-            access_token = jwt.encode(user.id, SECRET_KEY, algorithm = ALGORITHM)
             # 후에 프론트 사이드로 토큰과 status_code(200)를 반환해 로그인에 성공했음을 알림
+            access_token = jwt.encode({'user_id':user.id}, SECRET_KEY, algorithm = ALGORITHM)
             return JsonResponse({'MESSAGE':'SUCCESS', 'ACCESS_TOKEN': access_token}, status=200)
 
         # 입력값(아이디 혹은 비밀번호) 중 하나라도 빈 칸으로 들어오면 에러 반환
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
         # 유저가 입력한 아이디가 존재하지 않을 경우 에러 반환
         except User.DoesNotExist:
             return JsonResponse({'MESSAGE':'USER_DOES_NOT_EXIST'}, status=404)
